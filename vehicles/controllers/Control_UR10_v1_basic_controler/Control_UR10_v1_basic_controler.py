77f011350ea58d5654637caad53cada135dc0267
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2024-06-01 18:56:29 +0530

    forward kinematics

--- vehicles/controllers/Control_UR10_v1_basic_controler/Control_UR10_v1_basic_controler.py
+++ vehicles/controllers/Control_UR10_v1_basic_controler/Control_UR10_v1_basic_controler.py
@@ -1,69 +1,60 @@
-#
 #!/usr/bin/env python3.8
-from controller import Robot
+from controller import Robot, Keyboard
 
-
-# create the Robot instance.
+# Create the Robot instance
 robot = Robot()
 
-timestep = 64
+# Get the time step of the current world
+timestep = int(robot.getBasicTimeStep())
 
-# Define joint:
-joint = [robot.getDevice('shoulder_pan_joint'),
-            robot.getDevice('shoulder_lift_joint'),
-            robot.getDevice('elbow_joint'),
-            robot.getDevice('wrist_1_joint'),
-            robot.getDevice('wrist_2_joint'),
-            robot.getDevice('wrist_3_joint')]
-joint[0].setPosition(1.0)
-joint[0].setVelocity(1.0)
+# Enable the keyboard device
+keyboard = Keyboard()
+keyboard.enable(timestep)
 
-f=open('Trajectory.txt',"r")
-lines=f.readlines()
-data=[]
-for x in lines:
-    data.append(x.split(','))
-f.close()
-Trajectory = []
-for dt in data[1:]:
-    a = [float(i) for i in dt]
-    Trajectory.append(a)
-    
-joint[0].setPosition(Trajectory[1][0])        
-joint[1].setPosition(Trajectory[1][1])
-joint[2].setPosition(Trajectory[1][2])
-joint[3].setPosition(Trajectory[1][3])
-joint[4].setPosition(Trajectory[1][4])
-joint[5].setPosition(Trajectory[1][5])
-Step = 0
-i = 1
-Step_check = 70
-direction = 1
+# Define joints
+joints = [
+    robot.getDevice('shoulder_pan_joint'),
+    robot.getDevice('shoulder_lift_joint'),
+    robot.getDevice('elbow_joint'),
+    robot.getDevice('wrist_1_joint'),
+    robot.getDevice('wrist_2_joint'),
+    robot.getDevice('wrist_3_joint')
+]
+
+# for joint in joints:
+    # joint.setPosition(0.0)  # Initial position
+    # joint.setVelocity(1.0)  # Initial velocity
+
+print("Robot initialized and ready to receive inputs.")
+
+
+INCREAMENT = 0.01
+
+# Simulation step
 while robot.step(timestep) != -1:
-    Step += 1
-    if(Step >= Step_check):
-        Step_check += 2
-        i += direction
-        joint[0].setPosition(Trajectory[i][0])
-        # joint[0].setVelocity(0.5)
-            
-        joint[1].setPosition(Trajectory[i][1])
-        # joint[1].setVelocity(0.5)
-            
-        joint[2].setPosition(Trajectory[i][2])
-        # joint[2].setVelocity(0.5)
-            
-        joint[3].setPosition(Trajectory[i][3])
-        # joint[3].setVelocity(0.5)
-        
-        joint[4].setPosition(Trajectory[i][4])
-        # joint[4].setVelocity(0.5)
-            
-        joint[5].setPosition(Trajectory[i][5])
-        # joint[5].setVelocity(0.5)
-        print("true___",i)
-        if(i == len(Trajectory)-1 or i == -len(Trajectory)):
-            direction = direction * -1
-    print(Step)
-    pass
+    key = keyboard.getKey()
+    # print("key ", key)
+    if key != -1:
+        print(f"Key pressed: {key}")
 
+    if key == ord('A'):
+        print("A key pressed")
+        joints[0].setPosition(joints[0].getTargetPosition() + INCREAMENT)  # Increase shoulder_pan_joint position
+    elif key == ord('D'):
+        print("D key pressed")
+        joints[0].setPosition(joints[0].getTargetPosition() - INCREAMENT)  # Decrease shoulder_pan_joint position
+    elif key == ord("W") :
+        print("W key pressed")
+        joints[1].setPosition(joints[1].getTargetPosition() + INCREAMENT)  # Decrease shoulder_pan_joint position
+    elif key == ord("S"):
+        print("S key pressed")
+        joints[1].setPosition(joints[1].getTargetPosition() - INCREAMENT)  # Decrease shoulder_pan_joint position
+    elif key == ord("X"):
+        print("X key pressed")
+        joints[2].setPosition(joints[2].getTargetPosition() + INCREAMENT)  # Decrease shoulder_pan_joint position
+    elif key == ord("Z"):
+        print("Z key pressed")
+        joints[2].setPosition(joints[2].getTargetPosition() - INCREAMENT)  # Decrease shoulder_pan_joint position
+
+    # Add more key bindings for other joints as needed
+    pass
